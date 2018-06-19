6482198ec854fcfa140a463eb3176a2909e9292a
Larissa Ganaha <larissaganaha@gmail.com>
2018-06-19 17:21:48 -0300

    Arquivos arrumados e DatabaseService começando a ser feito

---
+++ WatchAndSee/WatchAndSee/Services/DatabaseService.swift
@@ -0,0 +1,23 @@
+//
+//  DatabaseService.swift
+//  WatchAndSee
+//
+//  Created by Larissa Ganaha on 19/06/18.
+//  Copyright © 2018 Matheus Garcia. All rights reserved.
+//
+
+import UIKit
+import Firebase
+
+//Classe Singleton que será responsável pela movimentação no banco de dados
+class DatabaseService: NSObject {
+    static let shared = DatabaseService()
+
+    var ref: DatabaseReference!
+
+    private override init() {
+        ref = Database.database().reference()
+    }
+
+
+}
