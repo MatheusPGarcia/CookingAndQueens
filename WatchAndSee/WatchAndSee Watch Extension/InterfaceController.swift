9297ca6e10044ea48757475d72d08ac4bb074f27
Unknown <matheuspgarcia@gmail.com>
2018-06-21 16:58:06 -0300

    Watch seending data to iPhone

--- WatchAndSee/WatchAndSee Watch Extension/InterfaceController.swift
+++ WatchAndSee/WatchAndSee Watch Extension/InterfaceController.swift
@@ -8,13 +8,34 @@
 
 import WatchKit
 import Foundation
+import WatchConnectivity
 
 // swiftlint:disable all
-class InterfaceController: WKInterfaceController {
+class InterfaceController: WKInterfaceController, WCSessionDelegate {
+
+    @available(watchOS 2.2, *)
+    public func session(_ session: WCSession, activationDidCompleteWith activationState: WCSessionActivationState, error: Error?) {
+        //Dummy Implementation
+    }
+    
+    var session : WCSession!
+    var value = 0
+
+    override init() {
+        super.init()
+        if (WCSession.isSupported()) {
+            session = WCSession.default
+            session.delegate = self
+            session.activate()
+        }
+    }
+
+    @IBOutlet var timeLabel: WKInterfaceLabel!
 
     override func awake(withContext context: Any?) {
         super.awake(withContext: context)
-        
+
+        timeLabel.setText("\(value)")
         // Configure interface objects here.
     }
     
@@ -27,5 +48,30 @@ class InterfaceController: WKInterfaceController {
         // This method is called when watch view controller is no longer visible
         super.didDeactivate()
     }
+
+    func sendTimer() {
+
+        do {
+            let time = ["Time" : value]
+            try session.updateApplicationContext(time)
+        } catch {
+            print("error")
+        }
+    }
+
+    @IBAction func buttonSendTimerWasPressed() {
+        sendTimer()
+    }
+
+    @IBAction func subWasPressed() {
+        value = value - 1
+        timeLabel.setText("\(value)")
+    }
+
+    @IBAction func addWasPressed() {
+        value = value + 1
+        timeLabel.setText("\(value)")
+    }
+    
 }
 // swiftlint:enable all
--- WatchAndSee/WatchAndSee/Controllers/ViewController.swift
+++ WatchAndSee/WatchAndSee/Controllers/ViewController.swift
@@ -7,16 +7,62 @@
 //
 
 import UIKit
+import WatchConnectivity
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, WCSessionDelegate {
+
+    @available(iOS 9.3, *)
+    public func sessionDidDeactivate(_ session: WCSession) {
+        //Dummy Implementation
+    }
+
+    @available(iOS 9.3, *)
+    public func sessionDidBecomeInactive(_ session: WCSession) {
+        //Dummy Implementation
+    }
+
+    @available(iOS 9.3, *)
+    public func session(_ session: WCSession,
+                        activationDidCompleteWith activationState: WCSessionActivationState,
+                        error: Error?) {
+        //Dummy Implementation
+    }
+
+    @IBOutlet weak var timeLabel: UILabel!
+
+    fileprivate let session: WCSession? = WCSession.isSupported() ? WCSession.default : nil
+
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+
+        configureWCSession()
+    }
+
+    override init(nibName nibNameOrNil: String?, bundle nibBundleOrNil: Bundle?) {
+        super.init(nibName: nibNameOrNil, bundle: nibBundleOrNil)
+
+        configureWCSession()
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    fileprivate func configureWCSession() {
+        session?.delegate = self
+        session?.activate()
+    }
+
+    internal func session(_ session: WCSession,
+                          didReceiveApplicationContext applicationContext: [String: Any]) {
+
+        let time = applicationContext["Time"] as? Int
+        //Use this to update the UI instantaneously (otherwise, takes a little while)
+        DispatchQueue.main.async() {
+
+            guard let timeValue = time else { return }
+
+            self.timeLabel.text = "time: \(timeValue)"
+        }
     }
 }
