58d33ef12366be39b830f90851ed91b03aa3787b
Unknown <matheuspgarcia@gmail.com>
2018-06-25 16:24:03 -0300

    Merge funcionando

--- WatchAndSee/WatchAndSee/Controllers/ViewController.swift
+++ WatchAndSee/WatchAndSee/Controllers/ViewController.swift
@@ -7,9 +7,8 @@
 //
 
 import UIKit
-import WatchConnectivity
 
-class ViewController: UIViewController, WCSessionDelegate {
+class ViewController: UIViewController {
 
     @IBOutlet weak var tableView: UITableView!
     @IBOutlet weak var highlightImage: UIImageView!
@@ -17,35 +16,6 @@ class ViewController: UIViewController, WCSessionDelegate {
 
     var databaseManager: DatabaseService!
     var recipies = [[Recipes]]()
-
-    @available(iOS 9.3, *)
-    public func sessionDidDeactivate(_ session: WCSession) {
-    }
-
-    @available(iOS 9.3, *)
-    public func sessionDidBecomeInactive(_ session: WCSession) {
-    }
-
-    @available(iOS 9.3, *)
-    public func session(_ session: WCSession,
-                        activationDidCompleteWith activationState: WCSessionActivationState,
-                        error: Error?) {
-    }
-
-    var value = 0
-    var session: WCSession!
-
-    @IBOutlet weak var valueLabel: UILabel!
-
-    required init?(coder aDecoder: NSCoder) {
-        super.init(coder: aDecoder)
-
-        if WCSession.isSupported() {
-            session = WCSession.default
-            session.delegate = self
-            session.activate()
-        }
-    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -67,50 +37,6 @@ class ViewController: UIViewController, WCSessionDelegate {
             }
             self.view.isUserInteractionEnabled = true
         })
-    }
-
-    fileprivate func updateLabel (value: Int) {
-        valueLabel.text = "\(value)"
-    }
-
-    internal func session(_ session: WCSession,
-                          didReceiveApplicationContext applicationContext: [String: Any]) {
-
-        let sessionValue = applicationContext["Value"] as? Int
-
-        DispatchQueue.main.async {
-
-            guard let safeValue = sessionValue else { return }
-
-            self.value = safeValue
-            self.updateLabel(value: self.value)
-        }
-    }
-
-    func sendValue() {
-
-        do {
-            let valueToSend = ["Value": value]
-            try session.updateApplicationContext(valueToSend)
-        } catch {
-            print("error")
-        }
-    }
-
-    @IBAction func subWasPressed(_ sender: UIButton) {
-
-        value -= 1
-        updateLabel(value: value)
-    }
-
-    @IBAction func addWasPressed(_ sender: UIButton) {
-
-        value += 1
-        updateLabel(value: value)
-    }
-
-    @IBAction func sendToWatchWasPressed(_ sender: UIButton) {
-        sendValue()
     }
 }
 // swiftlint:disable force_cast
