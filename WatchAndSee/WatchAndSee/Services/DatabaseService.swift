8e254065d49bfe135e0c3043f8ae79dd8508b5ee
Unknown <matheuspgarcia@gmail.com>
2018-07-05 12:04:51 -0300

    Deletando todas as referências ao firebase

--- WatchAndSee/WatchAndSee/Services/DatabaseService.swift
+++ WatchAndSee/WatchAndSee/Services/DatabaseService.swift
@@ -7,39 +7,22 @@
 //
 
 import UIKit
-import Firebase
 
 //Classe Singleton que será responsável pela movimentação no banco de dados
 class DatabaseService: NSObject {
 
     static var shared = DatabaseService()
     let parseRef = ParseManager()
-    var ref: DatabaseReference!
+//    var ref: DatabaseReference!
 
     private override init() {
         super.init()
 
-        // reference to Firebase Database
-        ref = Database.database().reference()
+//        ref = Database.database().reference()
     }
 
     //retrieve Data from database
     func createRecipeObject(completion: @escaping (_ response: [Recipes]?) -> Void) {
-        var ingredients = [Recipes]()
-
-        ref.child("Recipes").observeSingleEvent(of: .value) { snapshot in
-            for children in snapshot.children.allObjects {
-                if let childSnapshot = children as? DataSnapshot,
-                    let recipeJSON = childSnapshot.value as? [String: Any] {
-                    let recipe = self.parseRef.parseRecipe(recipeJSON)
-                    print(recipe)
-                    ingredients.append(recipe)
-                }
-
-            }
-            // guarantees it returns a value when it finishes retriving all data
-            completion(ingredients)
-        }
 
     }
 }
--- WatchAndSee/WatchAndSee/Supporting Files/AppDelegate.swift
+++ WatchAndSee/WatchAndSee/Supporting Files/AppDelegate.swift
@@ -8,7 +8,6 @@
 
 import UIKit
 import CoreData
-import Firebase
 import WatchConnectivity
 
 @UIApplicationMain
@@ -20,8 +19,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
-        
-        FirebaseApp.configure()
+
         setupWatchConnectivity()
 
         return true
